7d5331d67d5a39507d6e87323b346fce2f2aee85
Girish Ranganathan <girish.ranganathan11@gmail.com>
2022-07-31 11:43:38 -0400

    Adding files to FinalProject

---
+++ app/src/main/java/com/example/finalproject/recyclerRating.java
@@ -0,0 +1,18 @@
+package com.example.finalproject;
+
+public class recyclerRating {
+
+    String rating, title, description;
+
+    public String getRating() {
+        return rating;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
